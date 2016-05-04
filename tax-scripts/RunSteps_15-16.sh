c680fef6cd0ad0f5309863311d3c65ab2d940dc8
Robin Rohwer <Robin.Rohwer@gmail.com>
2016-05-04 10:29:21 -0500

    to RunSteps15-16.sh I double-checked the call, fixed error in 16, added bootstrap choice at the beginning instead of hardcoded, and added && at ends of lines

--- tax-scripts/RunSteps_15-16.sh
+++ tax-scripts/RunSteps_15-16.sh
@@ -7,18 +7,20 @@
 # Choose final pident.
 
 pident=("98")
+fwbootstrap=("85")
+ggbootstrap=("70")
 
 # 15
-Rscript find_classification_disagreements.R otus.$pident.taxonomy otus.general.taxonomy ids.above.$pident conflicts_$pident $pident 85 70 final
+Rscript find_classification_disagreements.R otus.$pident.taxonomy otus.general.taxonomy ids.above.$pident conflicts_$pident $pident $fwbootstrap $ggbootstrap final &&
 # 16
-Rscript plot_classification_improvement.R final.taxonomy.pvalues final.general.pvalues total.reads.per.seqID.csv plots
-mothur "#classify.seqs(fasta=otus.fasta, template=custom.fasta, taxonomy=custom.taxonomy, method=wang, probs=T, processors=2)"
-cat otus.custom.wang.taxonomy > otus.custom.taxonomy
-sed 's/[[:blank:]]/\;/' <otus.custom.taxonomy >otus.custom.taxonomy.reformatted
-mv otus.custom.taxonomy.reformatted otus.custom.taxonomy
-mkdir conflicts_forcing
-Rscript find_classification_disagreements.R otus.custom.taxonomy otus.$pident.85.70.taxonomy ids.above.$pident conflicts_forcing NA 85 70 forcing
-Rscript plot_classification_disagreements.R NA plots NA conflicts_forcing otus.custom.85.taxonomy
+Rscript plot_classification_improvement.R final.taxonomy.pvalues final.general.pvalues total.reads.per.seqID.csv plots &&
+mothur "#classify.seqs(fasta=otus.fasta, template=custom.fasta, taxonomy=custom.taxonomy, method=wang, probs=T, processors=2)" &&
+cat otus.custom.wang.taxonomy > otus.custom.taxonomy &&
+sed 's/[[:blank:]]/\;/' <otus.custom.taxonomy >otus.custom.taxonomy.reformatted &&
+mv otus.custom.taxonomy.reformatted otus.custom.taxonomy &&
+mkdir conflicts_forcing &&
+Rscript find_classification_disagreements.R otus.custom.taxonomy otus.$pident.$fwbootstrap.$ggbootstrap.taxonomy ids.above.$pident conflicts_forcing NA $fwbootstrap $ggbootstrap forcing &&
+Rscript plot_classification_disagreements.R NA plots conflicts_forcing otus.custom.$fwbootstrap.taxonomy &&
 
 printf 'Steps 15-16 have finished running.  When you are finished with all your analysis you can tidy up your working directory with step 17. \n \a'
 sleep .1; printf '\a'; sleep .1; printf '\a'; sleep .1; printf '\a'; sleep .1; printf '\a'; sleep .1; printf '\a'

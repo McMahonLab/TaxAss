0a9bd396d66b2da3d38cdf612e663986c0b3239f
Robin Rohwer <Robin.Rohwer@gmail.com>
2016-07-06 17:24:56 -0400

    in plot_improvement now it exports the stacked data along with the beside data (beside is what it already did).

--- tax-scripts/plot_classification_improvement.R
+++ tax-scripts/plot_classification_improvement.R
@@ -1,12 +1,11 @@
-# RRR 1/26/16
+# ---- RRR 1/26/16 ----
 
 # syntax for command line:
 
 # $ Rscript plot_classification_improvement.R final.taxonomy.pvalues final.general.pvalues total.reads.per.seqID plots
 
-#####
-# Accept arguments from the command line:
-#####
+
+# ---- Accept arguments from the command line: ----
 
 # userprefs <- commandArgs(trailingOnly = TRUE)
 # 
@@ -19,14 +18,15 @@ taxonomy.pvalues.path <- "../../take18playwith/final.taxonomy.pvalues"
 gg.pvalues.path <- "../../take18playwith/final.general.pvalues"
 reads.table.path <- "../../take18playwith/total.reads.per.seqID.csv"
 path.to.plots.folder <- "../../take18playwith/plots"
+taxonomy.names.path <- "../../take18playwith/final.taxonomy.names"
+gg.names.path <- "../../take18playwith/final.general.names"
 
-#####
-# Define functions to import data
-#####
+
+# ---- Define functions to import and format data ----
 
 import.pvalues <- function(FilePath){
-  taxonomy.pvalues.path <- FilePath
-  pvals <- read.table(file = taxonomy.pvalues.path, header = TRUE, sep = ",", colClasses = "character")
+  pvalues.path <- FilePath
+  pvals <- read.table(file = pvalues.path, header = TRUE, sep = ",", colClasses = "character")
   pvals[,2:8] <- apply(X = pvals[,2:8], MARGIN = 2, FUN = as.numeric)
   return(pvals)
 }
@@ -38,19 +38,32 @@ import.read.counts <- function(FilePath){
   return(reads)
 }
 
+import.and.order.names <- function(FilePath){
+  names.path <- FilePath
+  tax.names <- read.table(file = names.path, header = TRUE, sep = ",", colClasses = "character")
+  
+  index <- order(tax.names[ ,1])
+  tax.names <- tax.names[index, ]
+  
+  return(tax.names)
+}
 
-#####
-# Define functions to manipulate the data
-#####
+check.orders.match <- function(Vector1, Vector2){
+  # first check there are no duplicated names- can't have any ties in the order!
+  if (length(Vector1) != length(unique(Vector1)) | length(Vector2) != length(unique(Vector2)) | length(Vector1) != length(Vector2)){
+    cat("uh oh major problem, the number of seqIDs is different in your two tables!")
+  }
+  # second check that they are in the same order
+  if (all.equal(Vector1, Vector2)){
+    cat("orders match- good.")
+  }else{
+    cat("crap something's wrong, they didn't end up in the same order.  must fix.")
+  }
+}
 
 order.by.seqID.and.combine.tables <- function(ReadsTable, PvaluesTable){
   reads <- ReadsTable
   pvals <- PvaluesTable
-  
-  # first check there are no duplicated names- can't have any ties in the order!
-  if (length(pvals$seqID) != length(unique(pvals$seqID)) | length(reads$seqID) != length(unique(reads$seqID)) | length(pvals$seqID) != length(reads$seqID)){
-    cat("uh oh major problem, the number of seqIDs is different in your two tables!")
-  }
   
   # this orders numbers as characters, so 1, 10, 100.  oh well, only way to know it will work with character names for seqIDs
   index.reads <- order(reads[ ,1])
@@ -58,17 +71,16 @@ order.by.seqID.and.combine.tables <- function(ReadsTable, PvaluesTable){
   reads <- reads[index.reads, ]
   pvals <- pvals[index.pvals, ]
   
-  # check that they are in the same order now
-  if (all.equal(reads[,1], pvals[,1])){
-    cat("orders match- good.")
-  }else{
-    cat("crap something's wrong, they didn't end up in the same order.  must fix.")
-  }
+  check.orders.match(Vector1 = pvals$seqID, Vector2 = reads$seqID)
   
   pvals.with.reads <- cbind(reads, pvals[,2:8])
   
   return(pvals.with.reads)
 }
+
+
+
+# ---- Define functions to manipulate the data ----
 
 convert.to.name.presence.absence <- function(PvaluesTable){
   pvals <- PvaluesTable
@@ -88,10 +100,93 @@ convert.to.reads.presence.absence <- function(TrueFalseTable){
   return(pvals)
 }
 
+convert.to.unchanged.true.false <- function(FWnames, GGnames, TrueFalseTable){
+  fw.names <- FWnames
+  gg.names <- GGnames
+  otus.named.fw <- TrueFalseTable
+  check.orders.match(Vector1 = fw.names$seqID, Vector2 = gg.names$seqID)
+  check.orders.match(Vector1 = fw.names$seqID, Vector2 = otus.named.fw$seqID)
+  
+  unchanged <- otus.named.fw[ ,1:2]
+  # create a T/F table- T if the names match, F if they don't
+  unchanged <- cbind(unchanged, fw.names[ ,-1] == gg.names[ ,-1])
+  # adjust T/F table so also F if it was unclassified
+  unchanged[ ,-(1:2)] <- unchanged[ ,-(1:2)] * otus.named.fw[ ,-(1:2)]
+  
+  return(unchanged)
+}
 
-#####
-# Define functions to plot the data
-#####
+convert.to.renamed.true.false <- function(FWnames, GGnames, FW_TrueTalseTable, GG_TrueFalseTable){
+  fw.names <- FWnames
+  gg.names <- GGnames
+  otus.named.fw <- FW_TrueTalseTable
+  otus.named.gg <- GG_TrueFalseTable
+  
+  renamed <- otus.named.fw[ ,1:2]
+  # create a T/F table- T if the names DON'T match, F if they match
+  renamed <- cbind(renamed, fw.names[ ,-1] != gg.names[ ,-1])
+  # adjust T/F table so also F if it was unclassified by either
+  renamed[ ,-(1:2)] <- renamed[ ,-(1:2)] * otus.named.fw[ ,-(1:2)]
+  renamed[ ,-(1:2)] <- renamed[ ,-(1:2)] * otus.named.gg[ ,-(1:2)]
+  
+  return(renamed)
+}
+
+convert.to.newly.named.true.false <- function(FWnames, GGnames, FW_TrueTalseTable, GG_TrueFalseTable){
+  fw.names <- FWnames
+  gg.names <- GGnames
+  otus.named.fw <- FW_TrueTalseTable
+  otus.named.gg <- GG_TrueFalseTable
+  
+  # create table of those NOT named by gg only- T if it was unclassified by GG
+  otus.NOT.named.gg <- otus.named.gg[ ,1:2]
+  otus.NOT.named.gg <- cbind(otus.NOT.named.gg, otus.named.gg[ ,-(1:2)] == FALSE)
+  # the table of all given a name by FW workflow exists- T if it was classified by FW
+  newnamed <- otus.named.fw[ ,1:2]
+  # now adjust so T if NOT named by GG but NOW named by FW
+  newnamed <- cbind(newnamed, otus.named.fw[ ,-(1:2)] * otus.NOT.named.gg[ ,-(1:2)])
+  
+  return(newnamed)
+}
+
+get.beside.data <- function(GGTable, FWTable, Reads = TRUE){
+  ggpvals <- GGTable
+  fwpvals <- FWTable
+  
+  if (Reads == TRUE){
+    normalizer <- sum(fwpvals[ ,2])
+  }else{
+    normalizer <- nrow(ggpvals)
+  }
+  
+  gg.classified <- colSums(ggpvals[ ,3:9]) / normalizer * 100
+  fw.classified <- colSums(fwpvals[ ,3:9]) / normalizer * 100
+  
+  class.table <- rbind(gg.classified,fw.classified)
+  
+  return(class.table)
+}
+
+get.stacked.data <- function(Same, Better, New, Reads = TRUE){
+  unchanged <- Same
+  renamed <- Better
+  newly.named <- New
+  
+  if (Reads == TRUE){
+    normalizer <- sum(unchanged[ ,2]) # could be any of the 3 here
+  }else{
+    normalizer <- nrow(unchanged)
+  }
+  
+  tot.unchanged <- colSums(unchanged[ ,-(1:2)]) / normalizer * 100
+  tot.renamed <- colSums(renamed[ ,-(1:2)]) / normalizer * 100
+  tot.newly.named <- colSums(newly.named[ ,-(1:2)]) / normalizer * 100
+  
+  class.table <- rbind(tot.unchanged, tot.renamed, tot.newly.named)
+  return(class.table)
+}
+
+# Define functions to plot the data ----
 
 plot.num.classified <- function(GGTable, FWTable, Reads = TRUE, FolderPath, Tribe = FALSE, Kingdom = FALSE){
   ggpvals <- GGTable
@@ -159,10 +254,10 @@ fancy.barplot <- function(BesideData, StackedData, BarSpacing){
 }
 # fancy.barplot(BesideData = y, StackedData = z, BarSpacing = c(.1,1))
 
-#####
-# Use functions!
-#####
 
+# ---- Use functions! ----
+
+# get data to plot the overall improvement (beside) part of the barplot
 fw.pvals <- import.pvalues(FilePath = taxonomy.pvalues.path)
 gg.pvals <- import.pvalues(FilePath = gg.pvalues.path)
 
@@ -176,6 +271,34 @@ otus.named.gg <- convert.to.name.presence.absence(PvaluesTable = gg.pvals)
 
 reads.named.fw <- convert.to.reads.presence.absence(TrueFalseTable = otus.named.fw)
 reads.named.gg <- convert.to.reads.presence.absence(TrueFalseTable = otus.named.gg)
+
+beside.data.otus <- get.beside.data(FWTable = otus.named.fw, GGTable = otus.named.gg, Reads = FALSE)
+beside.data.reads <- get.beside.data(FWTable = reads.named.fw, GGTable = reads.named.gg, Reads = TRUE)
+
+# get data to plot the type of improvement (stacked) part of the barplot
+fw.names <- import.and.order.names(FilePath = taxonomy.names.path)
+gg.names <- import.and.order.names(FilePath = gg.names.path)
+check.orders.match(Vector1 = fw.names$seqID, Vector2 = gg.names$seqID)
+
+otus.unchanged <- convert.to.unchanged.true.false(FWnames = fw.names, GGnames = gg.names, TrueFalseTable = otus.named.fw)
+reads.unchanged <- convert.to.reads.presence.absence(TrueFalseTable = otus.unchanged)
+
+otus.reclassified <- convert.to.renamed.true.false(FWnames = fw.names, GGnames = gg.names, FW_TrueTalseTable = otus.named.fw, GG_TrueFalseTable = otus.named.gg)
+reads.reclassified <- convert.to.reads.presence.absence(TrueFalseTable = otus.reclassified)
+
+otus.newly.named <- convert.to.newly.named.true.false(FWnames = fw.names, GGnames = gg.names, FW_TrueTalseTable = otus.named.fw, GG_TrueFalseTable = otus.named.gg)
+reads.newly.named <- convert.to.reads.presence.absence(TrueFalseTable = otus.newly.named)
+
+stacked.data.otus <- get.stacked.data(Same = otus.unchanged, Better = otus.reclassified, New = otus.newly.named, Reads = FALSE)
+stacked.data.reads <- get.stacked.data(Same = reads.unchanged, Better = reads.reclassified, New = reads.newly.named, Reads = TRUE)
+
+barplot(stacked.data.otus)
+
+par(mfrow = c(1,2))
+barplot(beside.data.reads, beside = T, main = "reads", ylim = c(0,100))
+par(new = F)
+barplot(beside.data.otus, beside = T, main = "otus", ylim = c(0,100))
+
 
 plot.num.classified(FWTable = otus.named.gg, GGTable = otus.named.gg, Reads = FALSE, FolderPath = path.to.plots.folder)
 plot.num.classified(FWTable = reads.named.fw, GGTable = reads.named.gg, Reads = TRUE, FolderPath = path.to.plots.folder)

8df503ea9701e859439a8e87d53e34da62de8de9
Robin Rohwer <Robin.Rohwer@gmail.com>
2016-08-07 20:04:42 -0400

    added a table. But the text uses the freashtrain normalized by the named references and this is the data normalized by total references.

--- paper_figures/supp_fig_1_resolution_limits.R
+++ paper_figures/supp_fig_1_resolution_limits.R
@@ -8,3 +8,10 @@ barplot(summary.matrix[2,3:7], ylim = c(0,20), las = 3, main = "FreshTrain refer
 barplot(summary.plot.2[2,3:7], ylim = c(0,20), las = 3, main = "Lake Mendota reads matching multiple\nclassifications exactly", col = current.color)
 
 barplot(num.mismatches[3,3:7], ylim = c(0,20), las = 3, main = "FreshTrain references with\nunknown taxonomy", col = current.color)
+
+x <- cbind(summary.matrix[2,2:7], summary.plot.2[2,2:7], num.mismatches[3,2:7])
+colnames(x) <- c("FreshTrain references misclassified based on v4 region", 
+                 "Lake Mendota reads matching multiple classifications exactly",
+                 "FreshTrain references with unknown taxonomy")
+x
+write.csv(x = x, file = "~/Desktop/supptable1.csv")

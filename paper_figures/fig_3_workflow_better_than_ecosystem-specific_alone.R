f47c1bd5449af15ac2f3ee3959d4cb0500244cf8
Robin Rohwer <Robin.Rohwer@gmail.com>
2016-08-15 12:15:24 -0500

    added plot descriptions and how data was gathered.

--- paper_figures/fig_3_workflow_better_than_ecosystem-specific_alone.R
+++ paper_figures/fig_3_workflow_better_than_ecosystem-specific_alone.R
@@ -1,4 +1,27 @@
+# RRR 8-15-16 ----
 
+# fig 3 demonstrates that the workflow is better than using only an 
+# ecosystem-specific database by showing the extent of forcing.
+
+# fig 3a shows how forcing can mess up your fine-level taxa analysis
+# fig 3b shows how forcing can skew coarse-level taxa analyses
+
+# How am I defining forcing?
+# 1. classify everything with custom database
+# 2. look at the freshwater classifications of everything that the workflow put in greengenes
+# 3. ignore everything that's unclassified in freshwater classifications
+# 4. find all the classifications that are different from the workflow classifications-
+#    these are the ones I call "forced" into the wrong classification.
+
+# for fig 3a: baseline is freshwater rank abundance
+# 1. make a rank abundance plot of the top lineages as defined by the custom-only taxonomy
+#    (this leaves out "unclassified" from the taxa names)
+# 2.color the bars red for the OTUs that were not classified that way by the workflow
+
+# for fig 3b: baseline is workflow rank abundance
+# 1. make a rank abund of the top phyla in freshwater-only classifications
+# 2. make a rank abund of the top phyla in workflow classifications
+# 3. compare how different freshwater only is from workflow
 
 # from plot class dis:
 

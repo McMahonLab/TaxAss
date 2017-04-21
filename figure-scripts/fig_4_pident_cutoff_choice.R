1158daa2df75551ac59fc519550e73341934d1f7
Robin Rohwer <Robin.Rohwer@gmail.com>
2017-04-20 19:37:32 -0500

    edited fig 4 script to make paper quickie plots. only the percent classified included in this now. copied poster's stuff below so it shouldn't have changed

--- figure-scripts/fig_4_pident_cutoff_choice.R
+++ figure-scripts/fig_4_pident_cutoff_choice.R
@@ -1,38 +1,21 @@
 # RRR 8-15-16 ----
 
-# fig 4 is the plot showing how you can choose the percent identity cutoff to use
-#       based on the diminishing returns of percent database classified by ecosystem-specific
+# fig 4 is the plot showing how you can identify a good percent identity cutoff
 
-# fig 4a shows the choice for mendota unclustered reads (this looks smooth)
-# fig 4b shows the same unclustered plot for other ecosystems (some more dissimilar ones are rougher)
-# fig 4c shows the impact of clustering reads on the plot shape (they get rough where the cluster percent is)
+# fig 4a shows the percent identity cutoff that results in the maximum reads classified at different taxa levels
+# fig 4b shows the diminishing returns of lowering the cutoff as fewer reads at added to the FW classification
+
 # Supp Table 1 is the sanity check- make sure there aren't any phyla or classes being forced
 
 # ---- Define File Paths ----
 
-file.path.otu.summs <- "../../poster_mend_unclust/plots/conflict_summary_by_OTUs.csv"
-file.path.otu.perc.summs <- "../../poster_mend_unclust/plots/conflict_summary_by_percent_OTUs.csv"
-file.path.read.perc.summs <- "../../poster_mend_unclust/plots/conflict_summary_by_percent_reads.csv"
-output.folder.supp <- "~/Dropbox/Trina/8-20-16_ISME16_figures/bla"
-output.folder.fig4 <- "~/Dropbox/Trina/8-20-16_ISME16_figures/pident_choice_mendota_unclust.png"
+file.path.otu.summs <- "../../poster/poster_mend_unclust/plots/conflict_summary_by_OTUs.csv"
+file.path.otu.perc.summs <- "../../poster/poster_mend_unclust/plots/conflict_summary_by_percent_OTUs.csv"
+file.path.read.perc.summs <- "../../poster/poster_mend_unclust/plots/conflict_summary_by_percent_reads.csv"
 
-file.path.otu.summs <- "../../take_bogs_deblur/plots/conflict_summary_by_OTUs.csv"
-file.path.otu.perc.summs <- "../../take_bogs_deblur/plots/conflict_summary_by_percent_OTUs.csv"
-file.path.read.perc.summs <- "../../take_bogs_deblur/plots/conflict_summary_by_percent_reads.csv"
-output.folder.supp <- "~/Desktop/figures_8-16-16/SupTable1/bogs_"
-output.folder.fig4 <- "~/Desktop/figures_8-16-16/fig4b/bogs_"
+output.folder.supp <- "~/Desktop/test/supp"
+output.folder.fig4 <- "~/Desktop/test/fig4"
 
-file.path.otu.summs <- "../../take_danube_10/plots/conflict_summary_by_OTUs.csv"
-file.path.otu.perc.summs <- "../../take_danube_10/plots/conflict_summary_by_percent_OTUs.csv"
-file.path.read.perc.summs <- "../../take_danube_10/plots/conflict_summary_by_percent_reads.csv"
-output.folder.supp <- "~/Desktop/figures_8-16-16/SupTable1/danube_"
-output.folder.fig4 <- "~/Desktop/figures_8-16-16/fig4b/danube_"
-
-file.path.otu.summs <- "../../take_mendota_clust/plots/conflict_summary_by_OTUs.csv"
-file.path.otu.perc.summs <- "../../take_mendota_clust/plots/conflict_summary_by_percent_OTUs.csv"
-file.path.read.perc.summs <- "../../take_mendota_clust/plots/conflict_summary_by_percent_reads.csv"
-output.folder.supp <- "~/Desktop/figures_8-16-16/SupTable1/mendota_clust_"
-output.folder.fig4 <- "~/Desktop/figures_8-16-16/fig4c/mendota_98_"
 
 # ---- Define Functions ----
 
@@ -43,11 +26,14 @@ import.summary <- function(FilePath){
   return(sumry)
 }
 
-make.supplemental.table.1 <- function(ConflictsSum, FolderPath){
+make.supplemental.table.1 <- function(ConflictsSum, FolderPath = NULL){
   otus <- ConflictsSum[1:4, ]
-  file.name <- paste(FolderPath, "Supplemental_Table_1.csv", sep = "")
-  write.csv(x = otus, file = file.name, quote = FALSE, row.names = FALSE)
-  cat("made: ", file.name)
+  # only make a file if a folder is specified
+  if (!is.null(FolderPath)){
+    file.name <- paste(FolderPath, "Supplemental_Table_1.csv", sep = "")
+    write.csv(x = otus, file = file.name, quote = FALSE, row.names = FALSE)
+    cat("made: ", file.name)
+  }
   return(otus)
 }
 
@@ -61,48 +47,83 @@ trim.to.perc.classified <- function(ConflictSum){
   return(perc.class)
 }
 
-plot.perc.classified <- function(PercClass, Cutoff, FolderPath){
+plot.perc.classified <- function(PercClass, Cutoff, FilePath = NULL){
   pidents <- PercClass[ ,1]
   perc.class <- PercClass[ ,2]
   
-  plot.name <- paste(FolderPath, ".png", sep = "")
-  png(filename = plot.name, width = 7, height = 5, units = "in", res = 100)
+  # only save plot if folder specified
+  if (!is.null(FilePath)){
+    png(filename = FilePath, width = 7, height = 5, units = "in", res = 100)
+  }
   
-  # Set up and empty plot
+  # Set up an empty plot
   plot.title <- "Percent of Data Classified by Ecosystem-Specific Database"
   x.label <- "percent identity"
   y.label <- paste("Total Reads (%)")
   plot(x = pidents, y = perc.class, type = "n", main = plot.title, cex.main = 1, xlab = x.label, ylab = y.label)
   
   # Fill plot with beautiful data
-  lines(x = pidents, y = perc.class, col = "grey", lwd = 1.5)
-  points(x = pidents, y = perc.class, col = "grey", pch = 19, cex = .5)
+  lines(x = pidents, y = perc.class, col = "grey", lwd = 3)
+  points(x = pidents, y = perc.class, col = "grey", pch = 19, cex = 1)
   abline(v = Cutoff, col = "red", xpd = F)
   
-  dev.off()
-  cat("made plot: ", plot.name)
+  # stop exporting plot if you started to
+  if (!is.null(FilePath)){
+    unnecessary.message <- dev.off()
+    cat("made plot: ", FilePath)
+  }
 }
 
-# ---- Use Functions ----
+plot.max.classified.........
+
+# ---- Use Functions for quick looks ----
 
 otus <- import.summary(FilePath = file.path.otu.summs)
 
-make.supplemental.table.1(ConflictsSum = otus, FolderPath = output.folder.supp)
+make.supplemental.table.1(ConflictsSum = otus)
 
 reads <- import.summary(FilePath = file.path.read.perc.summs)
 
 reads.class <- trim.to.perc.classified(ConflictSum = reads)
 
-plot.perc.classified(PercClass = reads.class, Cutoff = 98, FolderPath = output.folder.fig4)
+plot.perc.classified(PercClass = reads.class, Cutoff = 98)
+
+
+# ---- PAPER ----
+
+draft.3.data <- "../../ME_GG/analysis/plots/conflict_summary_by_percent_reads.csv"
+draft.3.fig.4b <- "~/Dropbox/PhD/Write It/draft 3/draft_3_figure_files/fig_4b_percent_fw.png"
+
+reads <- import.summary(FilePath = draft.3.data)
+reads.class <- trim.to.perc.classified(ConflictSum = reads)
+plot.perc.classified(PercClass = reads.class, Cutoff = 99, FilePath = draft.3.fig.4b)
 
 
 # ---- ISME16 POSTER ----
 
-file.path.otu.summs <- "../../poster_mend_unclust/plots/conflict_summary_by_OTUs.csv"
 file.path.read.perc.summs <- "../../poster_mend_unclust/plots/conflict_summary_by_percent_reads.csv"
 output.folder.fig4 <- "~/Dropbox/Trina/8-20-16_ISME16_figures/pident_choice_mendota_unclust.png"
 
-# define functions above
+# define functions ----
+
+import.summary <- function(FilePath){
+  sumry <- read.csv(file = FilePath, header = FALSE, colClasses = "character")
+  sumry[1,1] <- "pident"
+  sumry[ ,-1] <- apply(X = sumry[ ,-1], MARGIN = 2, FUN = as.numeric)
+  return(sumry)
+}
+
+trim.to.perc.classified <- function(ConflictSum){
+  perc.class <- ConflictSum[-c(2:6), ]
+  future.colnames <- perc.class[ ,1]
+  perc.class <- perc.class[ ,-1]
+  perc.class <- apply(perc.class, 2, as.numeric)
+  perc.class <- t(perc.class)
+  colnames(perc.class) <- future.colnames
+  return(perc.class)
+}
+
+# use functions ----
 
 reads <- import.summary(FilePath = file.path.read.perc.summs)
 

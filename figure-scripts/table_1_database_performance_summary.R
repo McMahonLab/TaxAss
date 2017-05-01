039357b572bf704b78a19d39971f765fee4c1d33
Robin Rohwer <Robin.Rohwer@gmail.com>
2017-05-01 11:48:33 -0500

    organized figure and table scripts. re-named to match current paper draft. added placeholders for ones not yet separately generated. updated README

---
+++ figure-scripts/table_1_database_performance_summary.R
@@ -0,0 +1,5 @@
+# RRR
+# Table 1 compares the database performances at each taxa level for both metrics:
+# by % reads classified i.e. percent classified i.e. red/orange plot
+# by % otus classified i.e. alpha diversity i.e. blue/red plot
+
---
+++ figure-scripts/table_3_blast_hit_num.R
@@ -0,0 +1,3 @@
+# RRR
+# Table 3
+# BLAST hit chosen. This is generated directly. 

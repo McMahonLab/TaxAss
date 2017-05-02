9e2744d75ad84460787f9955952952dd0c821f30
Robin Rohwer <Robin.Rohwer@gmail.com>
2017-05-02 11:36:18 -0500

    Table 1 is complete.

--- figure-scripts/table_1_database_performance_summary.R
+++ figure-scripts/table_1_database_performance_summary.R
@@ -5,13 +5,34 @@
 
 # ---- Define File Paths ----
 
-run.folder <- "~/Desktop/TaxonomyTrainingSets/BLASTing/ME_GG/analysis/plots"
+run.folder <- "~/Desktop/TaxonomyTrainingSets/BLASTing/ME_plot_test/plots"
 
-chosen.pident <- 99
+output.file.path <- "~/Dropbox/PhD/Write It/draft 3/draft_3_figure_files/table_1.csv"
 
 # these filenames shouldn't change b/c scripts output as named:
-read.perc.taxass <- "Percent_OTUs_Classified_by_Pident.csv"
-read.perc.gg <- "Percent_OTUs_Classified_by_Pident.csv"
-read.perc.fw <- ""
+taxass.and.gg.alpha <- "step_15_5a_Improvement_over_general-only/alpha_diversity_TaxAss_vs_General.csv"
+taxass.and.gg.perc <- "step_15_5a_Improvement_over_general-only/WorkflowImprovement-BesideData-Reads.csv"
+fw.alpha.and.perc <- "step_15_5b_Improvement_over_custom-only/Custom-only_classification_stats.csv"
 
-names.tot.taxass <- "Percent_Reads_Classified_by_Pident.csv"
+# ---- Create Table 1 ----
+
+a <- read.csv(file = paste(run.folder, taxass.and.gg.alpha, sep = "/"))
+p <- read.csv(file = paste(run.folder, taxass.and.gg.perc, sep = "/"))
+f <- read.csv(file = paste(run.folder, fw.alpha.and.perc, sep = "/"))
+
+table.1 <- data.frame(TaxaLevel = c("Kingdom", "Phylum", "Class", "Order", "Family/Lineage", "Genus/Clade", "Species/Tribe"),
+                      Greengenes.perc = unlist(p[1,-1]), TaxAss.perc = unlist(p[2,-1]), FreshTrain.perc = f[ ,2],
+                      Greengenes.alpha = a[ ,3], TaxAss.alpha = a[ ,2], FreshTrain.alpha = f[ ,3])
+
+table.1 <- table.1[-1, ]
+
+table.1[ ,2:4] <- round(x = as.matrix(table.1[ ,2:4]), digits = 0)
+
+write.csv(x = table.1, file = output.file.path, quote = F, row.names = F)
+
+table.1
+cat("Made file: ", output.file.path)
+# in excel, make the boxes and column names pretty.
+# change font to helvetica
+# not sure how to format, but can save as .ps from excel
+# goal is width 6.875 inches

0c9d56909909d74f2dfe8a4bb740fbc5e75b4220
Robin Rohwer <Robin.Rohwer@gmail.com>
2017-05-26 14:43:23 -0500

    to step 16 cleanup wrapper, added that *.txt goes to analysis those are my terminal output files. there really aren't any other .txt documents.

--- tax-scripts/RunStep_16.sh
+++ tax-scripts/RunStep_16.sh
@@ -10,7 +10,7 @@
 # 16
 rm custom.db.* custom.8mer custom.custom* custom.tree* general.8mer general.general* general.tree* *wang* mothur.*.logfile otus.custom.blast* ids* otus.below*.fasta otus.above*.fasta otus.[0-9][0-9].taxonomy otus.[0-9][0-9][0-9].taxonomy otus.general.taxonomy otus.custom.taxonomy otus.custom.[0-9]* custom.general* *pvalues total* final*names
 mkdir scripts ; mv *.py *.R *.sh scripts
-mkdir analysis ; mv conflicts* plots analysis
+mkdir analysis ; mv conflicts* *.txt plots analysis
 mkdir data ; mv otus* data
 mkdir databases ; mv *.taxonomy *.fasta databases
 

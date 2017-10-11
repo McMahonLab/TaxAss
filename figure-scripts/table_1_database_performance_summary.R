9e81a54fa21dbfc5b9a9c7fa442baecd60c36609
Robin Rohwer <Robin.Rohwer@gmail.com>
2017-10-11 14:37:01 -0500

    updated paths to final data and notes on formatting after export

--- figure-scripts/table_1_database_performance_summary.R
+++ figure-scripts/table_1_database_performance_summary.R
@@ -5,9 +5,9 @@
 
 # ---- Define File Paths ----
 
-run.folder <- "~/Desktop/TaxonomyTrainingSets/BLASTing/ME_plot_test/plots"
+run.folder <- "~/Desktop/TaxAss-BatchFiles-go/Mendota/TaxAss-Mendota/analysis/plots/"
 
-output.file.path <- "~/Dropbox/PhD/Write It/draft 3/draft_3_figure_files/table_1.csv"
+output.file.path <- "~/Dropbox/PhD/Write It/draft 6/new_figs/Table_1.csv"
 
 # these filenames shouldn't change b/c scripts output as named:
 taxass.and.gg.alpha <- "step_15_5a_Improvement_over_general-only/alpha_diversity_TaxAss_vs_General.csv"
@@ -28,12 +28,18 @@ table.1 <- table.1[-1, ]
 
 table.1[ ,2:4] <- round(x = as.matrix(table.1[ ,2:4]), digits = 0)
 
+table.1
+
 write.csv(x = table.1, file = output.file.path, quote = F, row.names = F)
 
-table.1
 cat("Made file: ", output.file.path)
-# in excel, make the boxes and column names pretty.
-# change font to helvetica
-# not sure how to format, but can save as .ps from excel
-# goal is width 6.875 inches
+# in excel, make the boxes and column names pretty. 
+# Change to simply "Greengenes TaxAss FreshTrain" as repeated headers
+# add 2 column headers: "Percent Classified" over 1st 3 columns, "Taxonomic Richness" over second 3 columns
+# change font to helvetica, size 11, bold headers and labels, italicize and unbold superscripts
+# goal is width 6.875 inches. 1 page = 8.5 x 11, so set side margins to 0.82 and fit to page layout. 8.5 - 6.875 = 1.625, 1.625 / 2 = 0.8125
+# column widths are 10.33 and taxa label column is 14
 
+# not sure how to format, but can save as .ps from excel. 
+# for now did this: 
+# print to pdf from excel, open in ppt (word resizes), crop to size, select cropped image, save as image, as pdf

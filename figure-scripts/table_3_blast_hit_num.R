d8f140076d63fadce8db47b436a0628bd1478772
Robin Rohwer <Robin.Rohwer@gmail.com>
2017-10-24 11:16:27 -0500

    changed to round results instead of take significant digits. because columns weren't adding to 100 if 99.9 with 2 significant digist is 100%

--- figure-scripts/table_3_blast_hit_num.R
+++ figure-scripts/table_3_blast_hit_num.R
@@ -21,20 +21,23 @@ paper.table <- paper.table[ ,-(2:7)]
 colnames(paper.table) <- c("BLAST result", paste(95:100, "% ID", sep = ""))
 
 options(scipen = 999)
-paper.table[ ,2:7] <- signif(x = paper.table[ ,2:7], digits = 2) 
+paper.table[ ,2:7] <- round(x = paper.table[ ,2:7], digits = 3) 
 
 paper.table
 
 write.csv(x = paper.table, file = paper.file, quote = F, row.names = F)
-cat("Made file: ", output.file.path)
+cat("Made file: ", paper.file)
 
 # in excel, make the boxes and column names pretty. 
-# add column headers: "Percent Results with Highest Percent Identity"
-# add superscripts a b and c to explain blast result, % ID, and title.
-# change font to helvetica, size 11, bold headers and labels, italicize and unbold superscripts
-# fix significant digits to be by significant digits- don't understand wtf R thinks that means...
+# add column headers: "Percent Identity Cutoff Applied
+# merge Blast result header into 2 vertical cells.
+# add superscript a after Percent Identity Cutoff Applied
+# add superscript b after the top left value
+# change font to helvetica, size 11, bold headers and labels, italicize and unbold superscripts, center text
+# show only max digits needed per column to display smallest value
 # goal is width 6.875 inches. 1 page = 8.5 x 11, so set side margins to 0.82 and fit to page layout. 8.5 - 6.875 = 1.625, 1.625 / 2 = 0.8125
+# save as xlxs not csv or formatting is lost
 
-# not sure how to format, but can save as .ps from excel. 
-# for now did this: 
-# print to pdf from excel, open in ppt (word resizes), crop to size (unclick lock aspect ratio), select cropped image, save as image, as pdf
+# print to pdf from excel
+# open in ppt,unclick lock aspect ratio box, click on "reset size" to get original size back,
+# crop to size, select cropped image, save as image, as pdf

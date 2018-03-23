c9644c686872851b2ccfa65ab512d9bf7b6ee2a9
Robin Rohwer <Robin.Rohwer@gmail.com>
2018-03-23 11:38:37 -0500

    added stuff for making final figure version

--- arb-scripts/Marathonas_test/compare_full-length_to_taxass_results.R
+++ arb-scripts/Marathonas_test/compare_full-length_to_taxass_results.R
@@ -1,5 +1,5 @@
 # RRR 1/17/18
-# To satisfy reviewer #2, and also I think Pat Schloss bioRxiv review:
+# To satisfy reviewer #2, and also I think Pat Schloss bioRxiv review (and because they had a good point):
 # Concern was: in blue/red improvement plot, "truth" is TaxAss to support using TaxAss over GG alone.
 # The issue is that I'm testing TaxAss, so it shouldn't be used as the reference even though
 # this example is just meant to demonstrate obvious errors in the GG-only method
@@ -28,17 +28,17 @@ file.v4.ids <- userprefs[3]
 # for export
 folder.v4 <- userprefs[4]
 
-# # troubleshoot: ----
-# cat("CRAP COMMENT OUT FILE PATHS!!!")
-# # for import
-# file.arb.tax <- "../arb-scripts/Marathonas_test/mara.taxonomy"
-# 
-# file.v4.tax <- "../arb-scripts/Marathonas_test/v4_mara/otus.100.80.80.taxonomy"
-# file.v4.ids <- "../arb-scripts/Marathonas_test/v4_mara/ids.above.100"
-# 
-# # for export
-# folder.v4 <- "../arb-scripts/Marathonas_test/v4_mara/v4_results_100/"
-# # end troubleshoot ----
+# troubleshoot: ----
+cat("CRAP COMMENT OUT FILE PATHS!!!")
+# for import
+file.arb.tax <- "../arb-scripts/Marathonas_test/mara.taxonomy"
+
+file.v4.tax <- "../arb-scripts/Marathonas_test/v4_mara/otus.98.80.80.taxonomy"
+file.v4.ids <- "../arb-scripts/Marathonas_test/v4_mara/ids.above.98"
+
+# for export
+folder.v4 <- "../arb-scripts/Marathonas_test/v4_mara/v4_results_98/"
+# end troubleshoot ----
 
 # ---- Functions taken from find_classification_disagreements.R ----
 
@@ -476,6 +476,7 @@ make.stacked.bar <- function(){ # laxy calls from global env
   
   line.loc <- cumsum(results.v4[ ,7])
   line.loc <- line.loc - (.5 * results.v4[ ,7])
+  
   label.loc <- line.loc
   for (n in 2:(nrow(results.v4))){
     if (n < nrow(results.v4)){
@@ -509,8 +510,68 @@ make.stacked.bar <- function(){ # laxy calls from global env
   
 } 
 
+make.stacked.bar.for.manuscript <- function(){ # laxy calls from global env
+  # evenly spaced bar labels to match table
+  # colored differently ("under" is correct, yellow is db error, red is known error)
+  
+  label.cex <- 1.2
+  axis.cex <- 1.1
+  title.cex <- 1.2
+  excel.cex <- 1.7
+  
+  col.correct <- adjustcolor(col = "darkgreen")
+  col.under <- adjustcolor(col = rainbow(n = 20, v = .8)[4])
+  col.wrong <- adjustcolor(col = "darkred")
+  col.vector <- c(col.correct, col.correct, col.correct, col.correct, col.under, col.under, col.wrong, col.wrong)
+  
+  line.loc <- cumsum(results.v4[ ,7])
+  line.loc <- line.loc - (.5 * results.v4[ ,7])
+  label.loc <- seq(from = 30, to = 315, along.with = line.loc)
+  
+  max.val <- sum(results.v4[ ,1])
+  
+  par(mar = c(2,4,3,16))
+  bar.loc <- barplot(results.v4[ ,-(1:4)], col = col.vector, axisnames = F, axes = F)
+  
+  mtext(text = colnames(results.v4)[-(1:4)], side = 1, line = .5, at = bar.loc - c(.4,0,-.1), cex = label.cex)
+  
+  axis(side = 2, at = c(0, max.val), labels = F)
+  tic.loc <- axis(side = 2, labels = F)
+  label.scooch <- c(tic.loc + c(4, rep.int(x = 0, times = length(tic.loc) - 1)) , max.val + 10) # 0 up, 285 up
+  mtext(text = c(tic.loc,max.val), side = 2, line = .5, outer = F, at = label.scooch, cex = axis.cex)
+  mtext(text = "Number of Sequences", side = 2, line = 2.3, cex = label.cex)
+  
+  text(x = rep.int(x = 4.2, times = nrow(results.v4)), y = label.loc, labels = row.names(results.v4), xpd = T, adj = 0, cex = excel.cex, col = col.vector)
+  for (n in 1:nrow(results.v4)){
+    lines(x = c(3.62,4.15), y = c(line.loc[n], label.loc[n]), xpd = T)
+  }
+  
+  mtext(text = "TaxAss Accuracy", side = 3, line = 1.3, cex = title.cex)
+  
+  return(label.loc)
+} 
 
-
+make.example.table <- funciton(){ # all lazy calls to global env
+  
+  half.spacing <- (word.heights[2] - word.heights[1]) * 1/2
+  box.tops <- word.heights + half.spacing
+  box.bottoms <- word.heights - half.spacing
+  box.lefts <- 0
+  box.middles <- 5
+  box.rights <- 10
+  
+  # left boxes
+  for (b in 1:length(word.heights)){
+    rect(xleft = box.lefts, xright = box.middles, ybottom = box.bottoms[b], ytop = box.tops[b], xpd = T)
+  }
+  # right boxes
+  for (b in 1:length(word.heights)){
+    rect(xleft = box.middles, xright = box.rights, ybottom = box.bottoms[b], ytop = box.tops[b], xpd = T)
+  }
+  
+  plot(x = c(0,10), y = word.heights[c(1,length(word.heights))])
+  
+}
 
 # ---- Import & Format Data ----
 
@@ -573,7 +634,8 @@ write.csv(x = results.v4, file = paste(folder.v4, "summary_table.csv", sep = "/"
 
 pdf(file = paste(folder.v4, "stacked_bar.pdf", sep = "/"), width = 6.875, height = 3, family = "Helvetica", title = "Marathonas Validation", colormodel = "srgb")
 layout(mat = matrix(data = c(1,1,1,2,2), nrow = 1, ncol = 5))
-make.stacked.bar()
+word.heights <- make.stacked.bar.for.manuscript()
+make.example.table
 dev.off()
 
 

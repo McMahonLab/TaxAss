a371e6846cfefe8836df8c4515503edaba4f2b6d
Robin Rohwer <Robin.Rohwer@gmail.com>
2018-07-26 14:57:05 -0500

    for reformatting dada format, it removes any zero-read samples. Also, it makes an otus.count file to store the reads per sample for later. the seqtab file is annoying to work with because the sequences are column names, and taxass renames them when it also pulls out a fasta file. so makes sense to pull out all info in this one step and then not go back to the seqtab format at all.

--- tax-scripts/reformat_dada2_seqtabs.R
+++ tax-scripts/reformat_dada2_seqtabs.R
@@ -15,10 +15,11 @@ path.to.seqtab <- userprefs[1]
 fasta.output <- userprefs[2] 
 abund.output <- userprefs[3]
 
-# cat("fuck you forgot to comment out the file paths in reformat_dada2_seqtabs.R!")
-# path.to.seqtab <- "~/Desktop/dada2-meV45/seqtab_nochim.rds"
-# fasta.output <- "~/Desktop/dada2-meV45/taxass/otus.fasta"
-# abund.output <- "~/Desktop/dada2-meV45/taxass/otus.abund"
+cat("fuck you forgot to comment out the file paths in reformat_dada2_seqtabs.R!")
+path.to.seqtab <- "/Users/athena/Desktop/dada2-meV45/dada2/seqtab_nochim.rds"
+fasta.output <- "~/Desktop/otus.fasta"
+abund.output <- "~/Desktop/otus.abund"
+count.output <- "/Users/athena/Desktop/dada2-meV45/taxass_gg/data/otus.count"
 
 # ---- define functions ----
 
@@ -42,6 +43,13 @@ make.fasta.file <- function(dadatable, fasta.path){
   return(otu.names)
 }
 
+find.zero.samples <- function(tot.reads, otu.table){
+  read.stats <- boxplot(x = tot.reads, plot = F)
+  cat("These samples have outlier read counts. Samples with zero reads are being removed.\n",paste(names(read.stats$out), " : ", read.stats$out, sep = "", "\n"))
+  index <- which(tot.reads == 0)
+  return(index)
+}
+
 convert.to.rel.abund <- function(OTUs){
   sample.totals <- rowSums(OTUs)
   # vectors are applied to matrices by stepping down rows in a column
@@ -62,6 +70,13 @@ make.abund.file <- function(OTUs, FilePath){
   cat("Made file: ", FilePath, "\n")
 }
 
+make.count.file <- function(Count, FilePath){
+  Count <- cbind(names(Count), Count)
+  colnames(Count) <- c("Sample.Name", "Total.Reads")
+  write.table(x = Count, file = FilePath, quote = F, sep = "\t", row.names = F)
+  cat("Made file: ", FilePath, "\n")
+}
+
 # ---- use functions ----
 
 seqtab_nochim <- import.dada2.file(path = path.to.seqtab)
@@ -70,8 +85,22 @@ seqIDs <- make.fasta.file(dadatable = seqtab_nochim, fasta.path = fasta.output)
 
 colnames(seqtab_nochim) <- seqIDs
 
+read.count <- rowSums(seqtab_nochim)
+
+index <- find.zero.samples(tot.reads = read.count, otu.table = seqtab_nochim)
+if(length(index) > 0){
+  read.count <- read.count[-index]
+  seqtab_nochim <- seqtab_nochim[-index, ]
+}
+
 seqtab_nochim <- convert.to.rel.abund(OTUs = seqtab_nochim)
 
 seqtab_nochim <- reformat.for.taxass(OTUs = seqtab_nochim)
 
 make.abund.file(OTUs = seqtab_nochim, FilePath = abund.output)
+
+make.count.file(Count = read.count, FilePath = count.output)
+
+
+
+

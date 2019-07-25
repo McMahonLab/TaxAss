e86153651922ff1fb5081fad259116def12bfe26
Robin Rohwer <Robin.Rohwer@gmail.com>
2019-07-25 07:16:55 -0500

    updated cleanup batch script so that *.rds files are added to the "data" folder. (The raw data file input when coming from dada2 are rds files)

--- tax-scripts/RunStep_16.sh
+++ tax-scripts/RunStep_16.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # This is a sourceable bash script that runs step 16 of the workflow.  
-# Clean-up step
+# Clean-up step: intermediate files will be deleted, and all other files 
+# will be sorted into 4 folders: analysis, data, databases, scripts.
 # RRR 2/11/16
 
 # Make sure you're really done before deleting stuff.
-# But this leaves everything you'd need to re-analyze from the beginning.
+# This leaves everything you'd need to re-analyze from the beginning,
+# But removes things you'd need to re-analyze from the middle.
 
 # 16
 rm custom.db.* custom.8mer custom.custom* custom.tree* general.8mer general.general* general.tree* *wang* mothur.*.logfile otus.custom.blast* ids* otus.below*.fasta otus.above*.fasta otus.[0-9][0-9].taxonomy otus.[0-9][0-9][0-9].taxonomy otus.general.taxonomy otus.custom.taxonomy otus.custom.[0-9]* custom.general* *pvalues total* final*names
 mkdir scripts ; mv *.py *.R *.sh *.md *.Rmd *.html scripts
 mkdir analysis ; mv conflicts* *.txt plots analysis
-mkdir data ; mv otus* data
+mkdir data ; mv otus* *.rds data
 mkdir databases ; mv *.taxonomy *.fasta databases
 
 printf 'All Done! \n \a'

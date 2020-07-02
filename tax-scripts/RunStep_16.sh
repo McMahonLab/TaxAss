4b38a52558cd8f1867e2177b675eb8db37335e13
Robin Rohwer <Robin.Rohwer@gmail.com>
2020-07-02 16:49:07 -0500

    added $otus $custom and $general so that file names can be anything as input. also made it cheangeable from the command line (with arguments) instead of having to edit the top of the script.

--- tax-scripts/RunStep_16.sh
+++ tax-scripts/RunStep_16.sh
@@ -9,11 +9,23 @@
 # This leaves everything you'd need to re-analyze from the beginning,
 # But removes things you'd need to re-analyze from the middle.
 
+# ---- Receive input from terminal arguments --------------------------------------------------------------------------------------------
+
+otus=$1
+custom=$2
+general=$3
+
+printf "Running TaxAss step 16 (folder cleanup!).\n"
+printf "\notu filenames: $otus.fasta and $otus.abund\n"
+printf "custom database filenames: $custom.fasta and $custom.taxonomy\n"
+printf "general database filenames: $general.fasta and $general.taxonomy\n"
+
+# ---- What sparks joy? -----------------------------------------------------------------------------------------------------------------
 # 16
-rm custom.db.* custom.8mer custom.custom* custom.tree* general.8mer general.general* general.tree* *wang* mothur.*.logfile otus.custom.blast* ids* otus.below*.fasta otus.above*.fasta otus.[0-9][0-9].taxonomy otus.[0-9][0-9][0-9].taxonomy otus.general.taxonomy otus.custom.taxonomy otus.custom.[0-9]* custom.general* *pvalues total* final*names
+rm ${custom}.db.* ${custom}.8mer ${custom}.${custom}* ${custom}.tree* ${general}.8mer ${general}.${general}* ${general}.tree* *wang* mothur.*.logfile ${otus}.${custom}.blast* ids* ${otus}.below*.fasta ${otus}.above*.fasta ${otus}.[0-9][0-9].taxonomy ${otus}.[0-9][0-9][0-9].taxonomy ${otus}.${general}.taxonomy ${otus}.${custom}.taxonomy ${otus}.${custom}.[0-9]* ${custom}.${general}* *pvalues total* final*names
 mkdir scripts ; mv *.py *.R *.sh *.md *.Rmd *.html scripts
 mkdir analysis ; mv conflicts* *.txt plots analysis
-mkdir data ; mv otus* *.rds data
+mkdir data ; mv ${otus}* *.rds data
 mkdir databases ; mv *.taxonomy *.fasta databases
 
 printf 'All Done! \n \a'
--- tax-scripts/RunSteps_quickie.sh
+++ tax-scripts/RunSteps_quickie.sh
@@ -1,55 +1,73 @@
 #!/bin/bash
 
 # This performs the minimum amount of processing to get a final taxonomy file.
-# That means that you do not try different percent identity cutoffs to choose the best one.
-# That might make sense for you if you have already made a similarity choice, for example by
-# choosing a cutoff to cluster OTUs. Then just have pident match that cutoff.
-# In almost all of our test datasets we found a pident of 98 was best.
-# Note: this also skips the BLAST check (step 6). You could go back and just do that one.
+# That means that you do not check blast settings (step 6),
+# try different percent identity cutoffs to choose the best one (repeat of steps 1-13 and step 14),
+# or make plots to see how much better your dataset it now that you used TaxAss (step 15).
+# This also means that the OTU abundance table is not needed- it is only used for comparing pidents.
+# This is probably what most users want to do- just get a taxonomy table as quickly as possible!
 # Note: still run step 16 to tidy up.
 # Note: still gotta do the reformatting manually (step 0)
+# We recommend choosing the same pident as the sequence similarity used to define OTU clusters,
+# Or (preferred) we recommend choosing a pident of 98 and using unique sequences or dada/deblur ASVs.
 
-# USER CAN CHANGE THIS INPUT ---------------------------------
+# command line syntax:
+# ./RunSteps_quickie.sh otus custom general 98 80 80 2
 
-pident=("98")
-fwbootstrap=("80")
-ggbootstrap=("80")
-processors=("2")
+# ---- Receive input from terminal arguments --------------------------------------------------------------------------------------------
 
-# -------------------------------------------------------------
+otus=$1
+custom=$2
+general=$3
+pident=($4)
+fwbootstrap=$5
+ggbootstrap=$6
+processors=$7
+
+printf "Running TaxAss as quickly as possible- just generate the final taxonomy table!\n"
+printf "\notu filename: $otus.fasta\n"
+printf "custom database filenames: $custom.fasta and $custom.taxonomy\n"
+printf "general database filenames: $general.fasta and $general.taxonomy\n"
+printf "\nCreating the final taxonomy table with pident: $pident\n"
+printf "\nClassification bootstrap confidence cutoffs are $fwbootstrap %% for the custom classification and $ggbootstrap %% for the general classification.\n"
+printf "Using $processors processors.\n"
+
+
+# ---- Run TaxAss -----------------------------------------------------------------------------------------------------------------------
 
 # 1
-makeblastdb -dbtype nucl -in custom.fasta -input_type fasta -parse_seqids -out custom.db &&
+makeblastdb -dbtype nucl -in ${custom}.fasta -input_type fasta -parse_seqids -out ${custom}.db &&
 # 2
-blastn -query otus.fasta -task megablast -db custom.db -out otus.custom.blast -outfmt 11 -max_target_seqs 5 &&
+blastn -query ${otus}.fasta -task megablast -db ${custom}.db -out ${otus}.${custom}.blast -outfmt 11 -max_target_seqs 5 &&
 # 3
-blast_formatter -archive otus.custom.blast -outfmt "6 qseqid pident length qlen qstart qend" -out otus.custom.blast.table &&
+blast_formatter -archive ${otus}.${custom}.blast -outfmt "6 qseqid pident length qlen qstart qend" -out ${otus}.${custom}.blast.table &&
 # 4
-Rscript calc_full_length_pident.R otus.custom.blast.table otus.custom.blast.table.modified &&
+Rscript calc_full_length_pident.R ${otus}.${custom}.blast.table ${otus}.${custom}.blast.table.modified &&
 #5
-Rscript filter_seqIDs_by_pident.R otus.custom.blast.table.modified ids.above.${pident[0]} ${pident[0]} TRUE &&
-Rscript filter_seqIDs_by_pident.R otus.custom.blast.table.modified ids.below.${pident[0]} ${pident[0]} FALSE &&
+Rscript filter_seqIDs_by_pident.R ${otus}.${custom}.blast.table.modified ids.above.${pident[0]} ${pident[0]} TRUE &&
+Rscript filter_seqIDs_by_pident.R ${otus}.${custom}.blast.table.modified ids.below.${pident[0]} ${pident[0]} FALSE &&
 # 6 skip: blast stats plot
 # 7
-python find_seqIDs_blast_removed.py otus.fasta otus.custom.blast.table.modified ids.missing &&
+python find_seqIDs_blast_removed.py ${otus}.fasta ${otus}.${custom}.blast.table.modified ids.missing &&
 cat ids.below.${pident[0]} ids.missing > ids.below.${pident[0]}.all &&
 # 8
-python create_fastas_given_seqIDs.py ids.above.${pident[0]} otus.fasta otus.above.${pident[0]}.fasta &&
-python create_fastas_given_seqIDs.py ids.below.${pident[0]}.all otus.fasta otus.below.${pident[0]}.fasta &&
+python create_fastas_given_seqIDs.py ids.above.${pident[0]} ${otus}.fasta ${otus}.above.${pident[0]}.fasta &&
+python create_fastas_given_seqIDs.py ids.below.${pident[0]}.all ${otus}.fasta ${otus}.below.${pident[0]}.fasta &&
 # 9
-mothur "#classify.seqs(fasta=otus.above.${pident[0]}.fasta, template=custom.fasta,  taxonomy=custom.taxonomy, method=wang, probs=T, processors=$processors, cutoff=0)" &&
-mothur "#classify.seqs(fasta=otus.below.${pident[0]}.fasta, template=general.fasta, taxonomy=general.taxonomy, method=wang, probs=T, processors=$processors, cutoff=0)" &&
+mothur "#classify.seqs(fasta=${otus}.above.${pident[0]}.fasta, template=${custom}.fasta,  taxonomy=${custom}.taxonomy, method=wang, probs=T, processors=$processors, cutoff=0)" &&
+mothur "#classify.seqs(fasta=${otus}.below.${pident[0]}.fasta, template=${general}.fasta, taxonomy=${general}.taxonomy, method=wang, probs=T, processors=$processors, cutoff=0)" &&
 # 10
-cat otus.above.${pident[0]}.custom.wang.taxonomy otus.below.${pident[0]}.general.wang.taxonomy > otus.${pident[0]}.taxonomy &&
+cat ${otus}.above.${pident[0]}.${custom}.wang.taxonomy ${otus}.below.${pident[0]}.${general}.wang.taxonomy > ${otus}.${pident[0]}.taxonomy &&
 # 11 skip: general-only classification
 # 12
-sed 's/[[:blank:]]/\;/' <otus.${pident[0]}.taxonomy >otus.${pident[0]}.taxonomy.reformatted &&
-mv otus.${pident[0]}.taxonomy.reformatted otus.${pident[0]}.taxonomy &&
+sed 's/[[:blank:]]/\;/' <${otus}.${pident[0]}.taxonomy >${otus}.${pident[0]}.taxonomy.reformatted &&
+mv ${otus}.${pident[0]}.taxonomy.reformatted ${otus}.${pident[0]}.taxonomy &&
 # 13 skip: coarse taxonomy conflict checking
 # 14 skip: compare and choose a pident
 # 15
-Rscript find_classification_disagreements.R otus.$pident.taxonomy quickie ids.above.$pident conflicts_$pident $pident $fwbootstrap $ggbootstrap final &&
+Rscript find_classification_disagreements.R ${otus}.$pident.taxonomy quickie ids.above.$pident conflicts_$pident $pident $fwbootstrap $ggbootstrap final &&
 
+# ---- done (except step 16 cleanup) ----------------------------------------------------------------------------------------------------
 
 printf 'All done. If this finished without errors, RunStep_16.sh to delete intermediate files and organize the rest into convenient folders. \n \a'
 sleep .2; printf '\a'; sleep .2; printf '\a'; sleep .2; printf '\a'; sleep .2; printf '\a'; sleep .2; printf '\a'; 

5f04f2ff9f6bd7d24a28a9c1222d83a40a72f39b
Robin Rohwer <Robin.Rohwer@gmail.com>
2020-07-07 06:53:28 -0500

    moved readme's to analysis folder. imperfect clean-up because they should all be in different folders, but name matching will put them all together and analysis made the most sense as a catch all place.

--- tax-scripts/RunStep_16.sh
+++ tax-scripts/RunStep_16.sh
@@ -26,8 +26,8 @@ printf "general database filenames: $general.fasta and $general.taxonomy\n"
 # ---- What sparks joy? -----------------------------------------------------------------------------------------------------------------
 # 16
 rm ${custom}.db.* ${custom}.8mer ${custom}.${custom}* ${custom}.tree* ${general}.8mer ${general}.${general}* ${general}.tree* *wang* mothur.*.logfile ${otus}.${custom}.blast* ids* ${otus}.below*.fasta ${otus}.above*.fasta ${otus}.[0-9][0-9].taxonomy ${otus}.[0-9][0-9][0-9].taxonomy ${otus}.${general}.taxonomy ${otus}.${custom}.taxonomy ${otus}.${custom}.[0-9]* ${custom}.${general}* *pvalues total* final*names
-mkdir scripts ; mv *.py *.R *.sh *.md *.Rmd *.html scripts
-mkdir analysis ; mv conflicts* *.txt plots analysis
+mkdir scripts ; mv *.py *.R *.sh *.md scripts
+mkdir analysis ; mv conflicts* *.txt *.Rmd *.html *.xlsx plots analysis
 mkdir data ; mv ${otus}* *.rds data
 mkdir databases ; mv *.taxonomy *.fasta databases
 
